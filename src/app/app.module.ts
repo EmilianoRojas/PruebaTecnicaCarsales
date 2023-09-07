e255c4b791b9f12a0edde4f34265a9b18b366f11
EmilianoRojas <inversiones.socony@gmail.com>
2023-09-07 13:52:09 -0300

    se implementa anular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EpisodeListComponent } from './components/episode-list/episode-list.component';
 import { EpisodeDetailComponent } from './components/episode-detail/episode-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,11 @@ import { AppHeaderComponent } from './components/app-header/app-header.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
